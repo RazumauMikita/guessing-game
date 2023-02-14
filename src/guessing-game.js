1631ad6bd2be6a68a48587b8580dbbe78acbe216
Mikita <anykey2solo@gmail.com>
2023-02-14 16:16:48 +0300

    commend

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let mid = Math.ceil((this.min + this.max) / 2);
+        return mid;
     }
 
     lower() {
-
+        this.max = Math.ceil((this.min + this.max) / 2);
     }
 
     greater() {
-
+        this.min =  Math.ceil((this.min + this.max) / 2);
     }
 }
 
